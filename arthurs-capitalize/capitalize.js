417d4602d72c589d9600d8bc04140c068af8ce2f
Max Marker <maxmarker98@gmail.com>
2020-10-12 22:02:01 -0400

    did capitalization challenge from arthur

---
+++ arthurs-capitalize/capitalize.js
@@ -0,0 +1,22 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+    let wordArray = str.split(" ")
+    wordArray.forEach((word, i) => {
+        const first = word.charAt(0).toUpperCase()
+        const theRest = word.slice(1)
+        wordArray[i] = `${first}${theRest}`
+    });
+    const finalSentence = wordArray.join(' ')
+    console.log(finalSentence)
+}
+
+capitalize('hi there, how is it going?')
+capitalize('i love breakfast at bill miller bbq')
