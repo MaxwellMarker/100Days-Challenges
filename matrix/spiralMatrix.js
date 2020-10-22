7992a86bd4a0459b1b00a3a98e7764ef314af049
Max Marker <maxmarker98@gmail.com>
2020-10-22 00:39:23 -0400

    have an idea and started to write it but it is late

--- matrix/spiralMatrix.js
+++ matrix/spiralMatrix.js
@@ -15,4 +15,28 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-const matrix = (n) => {}
+const matrix = (n) => {
+    let spiMat = []
+    let nums = [];
+    for( let i = 1 ; i <= n*n ; i++ ){
+        nums.push(i)
+    }
+    for( let i = 0 ; i < n ; i++ ){
+        spiMat.push([])
+    }
+    const center = Math.floor(n/2)
+    let offset;
+    if(n % 2 === 0){
+        offset = 1
+    }else{
+        offset = 0
+    }
+    for( let i = (0 + offset) ; i < (center + offset) ; i++ ){
+        for( let x = 0 ; x <= i ; x++ ){
+            const index = (center+i-1);
+            spiMat[index].push(nums.pop())
+        }
+    }
+    console.log(spiMat)
+}
+matrix(2)
