377a40c386e3b642b46379576fd6ee8f7ff8013f
Max Marker <maxmarker98@gmail.com>
2020-10-23 23:59:35 -0400

    done with maxChar in js

--- max-char/max-char.js
+++ max-char/max-char.js
@@ -6,5 +6,24 @@
 // maxChar("apple 1231111") === "1"
 
 const maxChar = (str) => {
-
+    str = str.replace(' ', '');
+    const arr = str.split('');
+    let obj = {};
+    arr.forEach(char => {
+        if(obj[char]){
+            obj[char]++;
+        }else{
+            obj[char] = 1;
+        }
+    });
+    let maxCharacters = 0;
+    let mostCommon;
+    for(const char in obj) {
+        if(obj[char] > maxCharacters){
+            [maxCharacters, mostCommon] = [obj[char], char];
+        };
+    };
+    return mostCommon;
 }
+console.log(maxChar("abcc cccccd"))
+console.log(maxChar("apple 1231111"))
