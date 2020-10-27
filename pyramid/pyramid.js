a16452b85a92f5613d3efc0129346dfa1ceed8f5
Max Marker <maxmarker98@gmail.com>
2020-10-26 22:59:29 -0400

    done with pyramid

--- pyramid/pyramid.js
+++ pyramid/pyramid.js
@@ -15,5 +15,21 @@
 //       '#####'
 
 const pyramid = (n) => {
-    const l = (n * 2) - 1
+    const l = (n * 2) - 1;
+    for (let i = 0; i < n; i++) {
+        let hashes = 1 + (i * 2);
+        let spaces = l - hashes;
+        let output = '';
+        for (let x = 0; x < spaces/2; x++) {
+            output += ' ';
+        }
+        for (let x = 0; x < hashes; x++) {
+            output += '#';
+        }
+        for (let x = 0; x < spaces/2; x++) {
+            output += ' ';
+        }
+        console.log(output)
+    }
 }
+pyramid(30)
