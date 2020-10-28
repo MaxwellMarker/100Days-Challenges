b4ca4651dd36d296d558e827dd8a7334112f7ed5
Max Marker <maxmarker98@gmail.com>
2020-10-27 23:04:15 -0400

    finished reverse-integer problem

--- reverse-int/rev-int.js
+++ reverse-int/rev-int.js
@@ -9,7 +9,26 @@
 //   reverseInt(-90) === -9
 
 const reverseInt = (n) => {
-    let numString = `${n}`
-    console.log(numString.split(''))
-}
-reverseInt(-543)
+    let numString = `${n}`;
+    let numArr = numString.split('');
+    let negative = false;
+    if(numArr[0] === '-'){
+        negative = true;
+        numArr.shift();
+    };
+    let finalNum = 0;
+    numArr.forEach((num, i) => {
+        const value = parseInt(num) * (10 ** i);
+        if(negative){
+            finalNum -= value;
+        }else{
+            finalNum += value;
+        };
+    });
+    return finalNum;
+};
+console.log(reverseInt(15));
+console.log(reverseInt(981));
+console.log(reverseInt(500));
+console.log(reverseInt(-15));
+console.log(reverseInt(-90));
