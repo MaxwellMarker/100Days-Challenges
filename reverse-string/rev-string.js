342e192b2d8e28c0a514516f9a472620a50f6b5a
Max Marker <maxmarker98@gmail.com>
2020-10-28 21:26:46 -0400

    finished string reversing function

--- reverse-string/rev-string.js
+++ reverse-string/rev-string.js
@@ -6,4 +6,14 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-const reverse = (str) => {}
+const reverse = (str) => {
+    let arr = str.split('');
+    for (let i = 0; i < Math.floor(arr.length/2); i++) {
+        const second = arr.length - i - 1;
+        [arr[i], arr[second]] = [arr[second], arr[i]]
+    }
+    return arr.join('')
+}
+console.log(reverse('apple'))
+console.log(reverse('hello'))
+console.log(reverse('Greetings!'))
